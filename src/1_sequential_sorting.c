3b32803b9dbbdc9f2010fa83e7c0d3a620b15d32
dongyumengya <2394666017@qq.com>
2025-05-07 01:24:09 +0800

    branch02

--- src/1_sequential_sorting.c
+++ src/1_sequential_sorting.c
@@ -5,6 +5,7 @@
 
 //now i am in the branch-02
 //now i am in the branch-02-change and commit -2
+//now i am in the branch-02-change and commit -3
 
 void merge_double(size_t N, double A[N], double B[N], size_t begin, size_t mid, size_t end) {
     size_t i = begin;
